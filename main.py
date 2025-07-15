53ed3c76f09caf0397d4261f7e527aa9d288da11
Sreejith Hari <161014591+SreejithHari1881@users.noreply.github.com>
2025-07-15 12:56:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
     # Initialize Tracker
-    tracker = Tracker('models/best.pt')
+    tracker = Tracker('/content/best.pt')
+
 
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
