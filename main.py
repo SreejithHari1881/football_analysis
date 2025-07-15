43d4e0a49a2ac8482ad4a55deaadc9adc108d2fe
Sreejith Hari <161014591+SreejithHari1881@users.noreply.github.com>
2025-07-15 13:07:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
     # Initialize Tracker
-    tracker = Tracker('/content/best.pt')
+    tracker = Tracker('/content/football_analysis/models/best.pt')
+
 
 
     tracks = tracker.get_object_tracks(video_frames,
